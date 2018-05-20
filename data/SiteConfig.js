fa220b2ab505fe37c293b4db9e986f939ec6247f
renato ricco <renato.mur@gmail.com>
2018-05-20 18:50:13 -0300

    Minor changes on SiteConfig

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteTitle: 'renato ricco',
   siteTitleAlt: 'renatoricco',
-  siteTagline: 'links, videos e outras recomendações.',
+  siteTagline: 'Música, tecnologia, design, videos, links, glitches, notas pessoais e outras recomendações.',
   siteLogo: '',
   siteUrl: 'https://blog.renatoricco.com',
   pathPrefix: '/',
@@ -10,8 +10,10 @@ module.exports = {
   //
   userName: 'renato ricco',
   userAvatar: 'https://api.adorable.io/avatars/150/test.png',
+  userTagline:
+    'Renato Ricco é designer gráfico, creative coder e cansou de gerar conteúdo de graça pra rede social. Leia mais sobre mim ou entre em contato..',
   userDescription:
-    "Yeah, I like animals better than people sometimes... Especially dogs. Dogs are the best. Every time you come home, they act like they haven't seen you in a year. And the good thing about dogs... is they got different dogs for different people.",
+    'Eu sou designer gráfico, creative coder, astronomo amador e viciado em música, tecnologia, quadrinhos, ficção científica e mais um monte de coisa que nem lembro agora. Atualmente moro em São Paulo. Me manda uma mensagem se quiser tomar um café(ou uma cerveja) comigo.',
   //
   userGithub: 'rdricco',
   // userFacebook: 'r.ricco',
