e539f901c0f089575739cab8f179a519e57decee
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-16 01:19:22 +0000

    Deploy rdricco/blog.renatoricco to github.com/rdricco/blog.renatoricco.git:gh-pages

--- path---tags-experimental-1488ae0713842b72ba36.js
+++ path---tags-experimental-1488ae0713842b72ba36.js
@@ -1,2 +1,2 @@
-webpackJsonp([94370718237148],{2560:function(a,t){a.exports={data:{allBlogPostsMarkdown:{totalCount:1,edges:[{node:{id:"gcms-cjhwfah891stz0156dby9zrvq",date:"2018-06-01T00:00:00.000+00:00",title:"Espiral da Loucura by DEAFKIDS",slug:"espiral-da-loucura-by-deafkids",tags:["Bandcamp","Noise","Experimental"]}}]}},pathContext:{tag:"Experimental"}}}});
+webpackJsonp([94370718237148],{2559:function(a,t){a.exports={data:{allBlogPostsMarkdown:{totalCount:1,edges:[{node:{id:"gcms-cjhwfah891stz0156dby9zrvq",date:"2018-06-01T00:00:00.000+00:00",title:"Espiral da Loucura by DEAFKIDS",slug:"espiral-da-loucura-by-deafkids",tags:["Bandcamp","Noise","Experimental"]}}]}},pathContext:{tag:"Experimental"}}}});
 //# sourceMappingURL=path---tags-experimental-1488ae0713842b72ba36.js.map
