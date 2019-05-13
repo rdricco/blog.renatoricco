756a672f2e55d6a7c7b6ab83765b7e23b87664f0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-13 03:37:40 +0000

    Deploy rdricco/blog.renatoricco to github.com/rdricco/blog.renatoricco.git:gh-pages

--- path---punk-typography-7e9f1a6ad8aab02ba654.js
+++ path---punk-typography-7e9f1a6ad8aab02ba654.js
@@ -1,2 +1,2 @@
-webpackJsonp([98568306841400],{2445:function(t,p){t.exports={data:{blogPostsMarkdown:{id:"pckt-1084082089",slug:"punk-typography",title:"Punk Typography",date:"26/10/2015",tags:["via pocket"],category:"link",url:"http://punktype.tumblr.com/post/28117456365",html:"",childMarkdownRemark:{html:""}}},pathContext:{slug:"punk-typography"}}}});
+webpackJsonp([98568306841400],{2447:function(t,p){t.exports={data:{blogPostsMarkdown:{id:"pckt-1084082089",slug:"punk-typography",title:"Punk Typography",date:"26/10/2015",tags:["via pocket"],category:"link",url:"http://punktype.tumblr.com/post/28117456365",html:"",childMarkdownRemark:{html:""}}},pathContext:{slug:"punk-typography"}}}});
 //# sourceMappingURL=path---punk-typography-7e9f1a6ad8aab02ba654.js.map
--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-f8cf6ce7b90c465dce17.js","113087f0b81bef92832efb2900e39f44"],["/commons-b734b5c60d762b8dd59e.js","e7885493da33e28e0d6e70f3a4fe6082"],["/index.html","4a6c78ed66f4642b196f3a05131a62c1"],["/manifest.webmanifest","601266de10db2652ff22d60c8c4704bf"],["/offline-plugin-app-shell-fallback/index.html","f80dd177da93f27b70d1bf9d93ff0b6e"]];
+var precacheConfig = [["/app-3695b9e3e23454dbef97.js","d6bb43e4a401ea16a57eb9da41da2797"],["/commons-b734b5c60d762b8dd59e.js","dc6a23ecf45b7478cb2b3adc6d2adc76"],["/index.html","f307324e4adbda9e56b609badf61e3c7"],["/manifest.webmanifest","601266de10db2652ff22d60c8c4704bf"],["/offline-plugin-app-shell-fallback/index.html","23e40b0b4771926d169f2874d549fe90"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
