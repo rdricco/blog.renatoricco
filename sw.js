4a71d66687a5fcc5e931cb6d743f91039c2206e9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-22 03:42:18 +0000

    Deploy rdricco/blog.renatoricco to github.com/rdricco/blog.renatoricco.git:gh-pages

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-4ea0730a06094e4a863a.js","ade3b2819224fd4920cfa1c3507aa112"],["/commons-b734b5c60d762b8dd59e.js","44999e67921d0e46d57691d0bcb7fad6"],["/index.html","eeed4225b7fec9b9f283f0697356b855"],["/manifest.webmanifest","601266de10db2652ff22d60c8c4704bf"],["/offline-plugin-app-shell-fallback/index.html","9e7c640988fb204d16989195cee2b8f6"]];
+var precacheConfig = [["/app-318d17e82d96c4d65ae6.js","d062bf6e09c86f0d7fe3350ae22cda06"],["/commons-b734b5c60d762b8dd59e.js","44999e67921d0e46d57691d0bcb7fad6"],["/index.html","46f8e3c08cb42ea3f6361b29ddc40c60"],["/manifest.webmanifest","601266de10db2652ff22d60c8c4704bf"],["/offline-plugin-app-shell-fallback/index.html","7b537ee3a95d850dabcc420b1fa42caf"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
